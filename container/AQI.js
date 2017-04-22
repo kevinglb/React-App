83f4a67a7ff429ac002b140c2b2823af36ab77d2
Kevin <kevinglbx@gmail.com>
2017-04-22 11:16:45 +0800

    clear conflict

--- container/AQI.js
+++ container/AQI.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import {render} from 'react-dom';
-import Button from '../component/element/Button';
-import Input from '../component/element/Input';
-import {setCity,searchCityAqi} from '../actions/aqi';
-import {bindActionCreators} from 'redux';
-import { connect } from 'react-redux';
-import store from '../store/store';
 
-=======
 import React,{Component,PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import {render} from 'react-dom';
@@ -20,15 +10,11 @@ import { connect } from 'react-redux';
 import store from '../store/store';
 import AQIDiv from '../component/AQIDiv';
 import SearchList  from '../component/SearchList';
->>>>>>> 4ced95e1ac822359ecad0c160a8e1e35c3ea1a67
+
 
 class AQI extends React.Component{
 	constructor(props) {
   		super(props);
-<<<<<<< HEAD
-
-=======
->>>>>>> 4ced95e1ac822359ecad0c160a8e1e35c3ea1a67
   		this.state = {
   			searchCity: '',
   			getData: false
@@ -36,50 +22,10 @@ class AQI extends React.Component{
   		this.props = this.props;
   		this.handleChange = this.handleChange.bind(this);
   		this.handleSearch = this.handleSearch.bind(this);
-<<<<<<< HEAD
-  	}
-  	
-  	componentWillMount(){
-  		//store.dispatch(fetchBooks('javascript'));
-  	}
-
-  	handleChange(e){
-  		var city = e.target.value;
-  		this.setState({searchCity: city});
-  		store.dispatch(setCity(city));
-  	}
-
-  	handleSearch(e){
-  		var city = this.state.searchCity;
-  		store.dispatch(searchCityAqi(city));
-  	}
-
-  	componentWillReceiveProps(){
-  		const {data,getData} = this.props;
-  		this.setState({getData:getData});
-  	}
-
-	render(){
-		
-		
-		
-		return(
-			<div className="container">
-				<div className="col-lg-6 col-lg-offset-3">
-					<div className="input-group col-lg-6 ">
-						<Input type="text" className="form-control" value={this.state.searchCity} onChange={this.handleChange} placeholder="Search City"/>
-						<span className="input-group-btn  ">
-        			<Button className="btn glyphicon glyphicon-search" type="button" onClick={this.handleSearch}/>
-      				</span>
-					</div>
-
-          <div className="result-group">
-          </div>
-				</div>
-=======
       this.handleKeyPress = this.handleKeyPress.bind(this);
 
   }
+
   componentWillMount(){
   		//store.dispatch(fetchBooks('javascript'));
   }
@@ -160,7 +106,7 @@ class AQI extends React.Component{
           // </div>
         </div>
 				
->>>>>>> 4ced95e1ac822359ecad0c160a8e1e35c3ea1a67
+
 			</div>
 		);
 	}
@@ -168,24 +114,19 @@ class AQI extends React.Component{
 
 function mapStateToProps(state){
 	return {
-<<<<<<< HEAD
-		data: state.aqi.aqiData,
-		getData: state.aqi.getData
-=======
+
 		aqiData: state.aqi.aqiData,
     fillList: state.aqi.fillList,
     getAqiData: state.aqi.getAqiData,
     getFillList: state.aqi.getFillList
->>>>>>> 4ced95e1ac822359ecad0c160a8e1e35c3ea1a67
+
 	};
 }
 
 function mapDispatchToProps(dispatch) {
-<<<<<<< HEAD
-  return bindActionCreators({searchCityAqi,setCity}, dispatch);
-=======
+
   return bindActionCreators({searchAQI,setCity,fillStation}, dispatch);
->>>>>>> 4ced95e1ac822359ecad0c160a8e1e35c3ea1a67
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(AQI);
